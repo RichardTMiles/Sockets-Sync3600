c4bcce1d99a5496920b649d5af19d82aeb683f39
BAdams999 <bryana825@gmail.com>
2016-12-07 22:58:26 -0600

    Add files via upload
    Added cpu usage check to cli.c

--- cli.c
+++ cli.c
@@ -12,6 +12,18 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
+//quick power function because I wanted one for ints
+int power(int x, int y)
+{
+    int result = 1;
+    int c = 0;
+    for(; c < y; c++)
+        result *= x;
+    return result;
+}
+
+
+
 
 void error(const char *msg) {
     perror(msg);
@@ -30,6 +42,27 @@ int main(int argc, char *argv[]) {
     int EOFileArray;                /* descriptors up to maxfd-1 polled */
     int SocketsReady;               /* # descriptors ready. */
     ssize_t bytesRead;              /* # chars on read() . */
+
+    FILE *fp;                       /*pointer to grab ouput of a ps command*/
+    char path[200];                 //string to hold said output
+    int cpusage = 0;                //int to hold cpu usage
+    char command[100] = "ps -o\"%C\" -p       | sed -n '2p'";
+    pid_t pid = getpid();           //id of this process
+    int ipid = pid;                 //cast to an int
+    int i = 4;                      //power of 10 ar which to start
+    int conv;                       //converter to a single digit
+    int c = 13;                     //counter starting at the spot in the command string where pid goes
+    int maxuse                      //maximum allowable cpu usage
+
+    //filling out pid of command string
+    while(ipid > 0)
+    {
+        conv = ipid / power(10, i);
+        command[c] = conv + 48;
+        ipid -= conv * power(10, i);
+        i--;
+        c++;
+    }
 
     fd_set FileArray;     /* Set of file descriptors to poll */
 
@@ -54,8 +87,35 @@ int main(int argc, char *argv[]) {
     /*Connect to the server.*/
     if (connect(ServerSocket, (struct sockaddr *) &ServerStructure, sizeof(ServerStructure)) < 0) { perror("connect"); exit(1); }
 
+    //prompt user for maximum allowable cpu usage
+    printf("What is the maximum cpu usage you would like to alot to this program?\n");
+    scanf("%d", maxuse);
+    //keep prompting until the number is acceptable
+    while (maxuse < 1 || maxuse > 100) {
+        printf("Please input a number between 1 and 100\n");
+        scanf("%d", maxuse);
+    }
+
     int total = 0;
     while (1) {
+
+        //check current cpu usage
+        fp = popen(command, "r");
+        if (fp == NULL) {
+            printf("Failed to run command\n" );
+            exit(1);
+        }
+        else if (fgets(path, sizeof(path) - 1, fp) != NULL) {
+            cpusage = atoi(&path[0]);
+            printf("current usage: %d\n", cpusage);
+
+            if(cpusage > maxuse) {
+                /**********************************************
+                  insert cpu overusage stuf here 
+                ***********************************************/
+            }
+        }
+
 
         /* Wait for some input. */
         printf("Enter CLIENT 2 Data: ");
